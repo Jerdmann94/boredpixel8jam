2e600a82f7a374fe003901522c47c7c9e6020630
Zach Erdmann <zach.erdmann@morningstar.com>
2021-07-17 11:49:43 -0500

    Add linter, fix build

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 import Phaser from 'phaser';
-import Scene from './js/scene.js'
-import PhaserMatterCollisionPlugin from 'phaser-matter-collision-plugin'
-
+import Scene from './js/scene.js';
+import PhaserMatterCollisionPlugin from 'phaser-matter-collision-plugin';
 
 const config = {
-    type: Phaser.AUTO,
-    width: 448,
-    height: 224,
-    backgroundColor: "#000c1f",
-    parent: "game-container",
-    scene: Scene,
-    pixelArt: true,
-    physics: { default: "matter" },
-    plugins: {
-        scene: [
-            {
-                plugin: PhaserMatterCollisionPlugin, // The plugin class
-                key: "matterCollision", // Where to store in Scene.Systems, e.g. scene.sys.matterCollision
-                mapping: "matterCollision" // Where to store in the Scene, e.g. scene.matterCollision
-            }
-        ]
-    }
+  type: Phaser.AUTO,
+  width: 448,
+  height: 224,
+  backgroundColor: '#000c1f',
+  parent: 'game-container',
+  scene: Scene,
+  pixelArt: true,
+  physics: { default: 'matter' },
+  plugins: {
+    scene: [
+      {
+        plugin: PhaserMatterCollisionPlugin, // The plugin class
+        key: 'matterCollision', // Where to store in Scene.Systems, e.g. scene.sys.matterCollision
+        mapping: 'matterCollision', // Where to store in the Scene, e.g. scene.matterCollision
+      },
+    ],
+  },
 };
 
 const game = new Phaser.Game(config);
